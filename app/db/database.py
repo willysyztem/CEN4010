4a52b28f54f161809c3524de81c3d57a1ac0a9eb
William Valido <valido.william@gmail.com>
2022-02-16 16:40:59 -0500

    Development (#21)
    * changes to github link
    
    * changes
    
    * Profile management (#20)
    
    * changed my github link
    
    * changes to readme
    
    * changes
    
    * added lucho
    
    * ..
    
    * added sprints
    
    * init branch
    
    Co-authored-by: Willy Valido <valido.william@gmailcom>
    
    Co-authored-by: Willy Valido <valido.william@gmailcom>

---
+++ app/db/database.py
@@ -0,0 +1,21 @@
+from venv import create
+from click import echo
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# DB info from settings
+from config.settings import settings
+
+# DB URL
+SQLALCHEMY_DATABASE_URL = settings.POSTGRES_URL
+
+# DB ENGINE
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+
+# DB SESSION
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+# DB BASE
+Base = declarative_base()
+
