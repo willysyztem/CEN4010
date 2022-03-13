e903af4eb6dd9afe42900a26a9548828dae0eb3e
William Valido <valido.william@gmail.com>
2022-03-13 11:55:22 -0400

    added wishlist limit and uniqueness

--- app/routers/wishlist.py
+++ app/routers/wishlist.py
@@ -8,6 +8,8 @@ from db.database import get_db
 import models.wishlist as models, schemas.wishlist as schemas
 import utils, oauth2
 
+MAX_ALLOWED_WISHLIST = 3
+
 router = APIRouter(
     prefix = '/api/wishlist',
     tags=['Wish List Management']
@@ -15,18 +17,22 @@ router = APIRouter(
 
 @router.post('/{username}')
 def create_wish_list(username, new_wish_list: schemas.WishList, db: Session = Depends(get_db)):
-    try:
-        wish_list = models.WishList(
-            name = new_wish_list.name,
-            books = new_wish_list.books,
-            owner_username = username
-        )
-        db.add(wish_list)
-        db.commit()
-        db.refresh(wish_list)
-        return {'detail': f'Wish List created for {wish_list.owner_username}'}
-    except Exception as e:
-        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f'Could not create wish list => {e}')
+    existing_wishlist = db.query(models.WishList).filter(models.WishList.owner_username == username).all()
+    if len(existing_wishlist) < MAX_ALLOWED_WISHLIST:
+            wish_list = models.WishList(
+                name = new_wish_list.name,
+                books = new_wish_list.books,
+                owner_username = username
+            )
+            for wishlist in existing_wishlist:
+                if wishlist.name == wish_list.name:
+                    raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f'Wishlist name already exists => {wish_list.name}')
+            db.add(wish_list)
+            db.commit()
+            db.refresh(wish_list)
+            return {'detail': f'Wish List created for {wish_list.owner_username}'}
+    else:
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f'Could not create wish list => Wishlist limit reached!')
 
 @router.get('/{username}', response_model=List[schemas.ShowWishList])
 def get_wish_list(username, db: Session = Depends(get_db)):
